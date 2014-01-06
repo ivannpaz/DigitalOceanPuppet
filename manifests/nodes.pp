c2e39789dcdee9bc40051e1c1aa4e0536709566e
ivan <ivannpaz@gmail.com>
2014-01-07 00:34:56 +0100

    Remove the node config in favor of just one server

--- manifests/nodes.pp
+++
@@ -1,9 +0,0 @@
-node default {
-
-    include sudo
-    include users
-    include ssh
-    include git
-    include nginx
-
-}
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,19 @@
-import 'nodes.pp'
+class server_config {
+
+    #forge modules
+    include ::stdlib
+    include ::ntp
+    include ::apt
+    include ::ruby
+    include ::nodejs
+
+    #server specific configs
+    include sudo
+    include users
+    include ssh
+    include git
+    include nginx
+
+}
+
+include server_config
