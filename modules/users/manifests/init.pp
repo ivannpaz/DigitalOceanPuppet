eeb461d131d54a91a311deecad7736d630ad2b5d
ivan <ivannpaz@gmail.com>
2014-01-07 02:08:31 +0100

    Use Hiera for variable configuration

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -9,11 +9,13 @@ class users {
         shell => '/bin/bash',
     }
 
+    $ssh = hiera('ssh', false)
+
     ssh_authorized_key { 'deployer':
         ensure => 'present',
         user => 'deployer',
         type => 'rsa',
-        key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAwzJurskQS5PVpeOuCwDLnbpT7N4+6bdRQXNENGLmMfFZDEQnX3nO+OEnm8HgvA0q7jw8zVWND/US29DhDM1JeccyP7qqCwgzKBEXyjNhgygUKjreYeQ4dLYy5js9h7YN6Yoqmv0F1Iq9aU+IpXdNjTvJN8+tzxCknzBuOwQKJx8zTs+xCi4/y3xwb5+R1ZUNiR4wqTwpmVwRjxo4lzTkHquL8MnLhkwF7JyLVUSsmlje+A/qBiFlYlJGrip9DLVt0DUo6JQbMaVWppD2Hni05IGgIHFaUipdrZdfJOuhcup+gXu6UfX7mlBuz05533fZaLRE00lszxBFS8yFcoesYQ==',
+        key => $ssh['public_key'],
     }
 
 }
